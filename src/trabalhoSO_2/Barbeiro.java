5f3dff01ef99ab9206638489a32e20ace4992a24
titi1975 <tiagobezerra.trb@gmail.com>
2025-06-29 19:11:19 -0300

    update to barber class

--- src/trabalhoSO_2/Barbeiro.java
+++ src/trabalhoSO_2/Barbeiro.java
@@ -1,9 +1,11 @@
 package trabalhoSO_2;
 
+import java.util.LinkedList;
+
 public class Barbeiro extends Thread {
 	int cliente;
 	long tempoAtendimento;
-
+	LinkedList clientes;
 	
 	public Barbeiro() {
 	}
@@ -13,11 +15,27 @@ public class Barbeiro extends Thread {
 	 * @param index
 	 * @param cadeiras
 	 */
-	public void run(int cliente){ // tell it to my heart tell me i'm the only one
+	// tipos de cliente 1-oficial 2-sargento 3-cabo 0-pausa
+	
+	public double cutHair(Cliente cliente) {
+		double random = Math.random();
+		
+		switch (cliente.tipo) {
+			case 1:
+				return (random*(2000) + 4000);
+			case 2:
+				return (random*(2000) + 2000);
+			case 3:
+				return (random*(2000) + 1000);
+			default:
+				return 0;
+		}
+	}
+	
+	
+	public void run(Cliente cliente){ // tell it to my heart tell me i'm the only one
 		try {
-			double rng = Math.random();
-			if (cliente < 3) tempoAtendimento =  (long)(rng*(2000) + 1000*cliente);
-			else tempoAtendimento = (long)(rng*(2000) + 4000);
+			tempoAtendimento = (long) cutHair(cliente);
 			wait(tempoAtendimento);
 		}
 		catch (Exception e) {
